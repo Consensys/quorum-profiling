935a238597fcef323360904bd781e2554df29993
amalraj.manigmail.com <amalraj.manigmail.com>
2020-05-06 23:21:20 +0800

    code improvements

--- stresstest-aws/jmeter-test/bootstrap.sh
+++ stresstest-aws/jmeter-test/bootstrap.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
+notUp=0
 for host in `cat host_acct.csv |cut -d"," -f1 | grep -v url`
 do
-  counter=0
-  while [ $counter -eq 0 ]
+  res=0
+  tries=1
+  triesCnt=60
+  while [ $tries -lt $triesCnt ]
   do
-    echo "checking connectivity for node $host ..."
-    counter=$( curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_accounts","params":[],"id":1}' -H "Content-Type: application/json" $host:8545|grep result |wc -l )
-    echo $counter
+    echo "checking connectivity for node $host ... tries $tries"
+    res=$( curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_accounts","params":[],"id":1}' -H "Content-Type: application/json" $host:8545|grep result |wc -l )
+    echo $res
+    if [ $res -gt 0 ]
+    then
+        echo "node $host is up"
+        break
+    fi
     sleep 1
+    tries=$( expr $tries + 1 )
   done
-  echo "node $host is up"
+
+  if [ $res -eq 0 ]
+  then
+    echo "node $host is not up"
+    notUp=1
+    break
+  fi
+
 done
-echo "all nodes are up"
 
-./start-jmeter-test.sh
+if [ $notUp -eq 1 ]
+then
+    echo "All nodes are not up. Fix the nodes and then start jmeter test(./start-jmeter-test.sh) and tps monitor(./start-tps.sh) manually."
+else
+    echo "All nodes are up"
+    ./start-jmeter-test.sh
+    ./start-tps.sh
+fi
 
-./start-tps.sh
