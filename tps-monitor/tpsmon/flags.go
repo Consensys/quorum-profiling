ab952e3f031ba6a6b345ecf11c036e424716f239
amalraj mani <amalraj.manigmail.com>
2020-07-02 12:56:56 +0800

    update usage

--- tps-monitor/tpsmon/flags.go
+++ tps-monitor/tpsmon/flags.go
@@ -5,94 +5,94 @@ import "gopkg.in/urfave/cli.v1"
 var (
 	AwsMetricsEnabledFlag = cli.BoolFlag{
 		Name:  "awsmetrics",
-		Usage: `aws metrics enabled`,
+		Usage: `AWS metrics enabled`,
 	}
 
 	AwsRegionFlag = cli.StringFlag{
 		Name:  "awsregion",
-		Usage: `aws region`,
+		Usage: `AWS region`,
 	}
 
 	AwsNwNameFlag = cli.StringFlag{
 		Name:  "awsnetwork",
-		Usage: `aws network name`,
+		Usage: `AWS network name`,
 	}
 
 	AwsInstanceFlag = cli.StringFlag{
 		Name:  "awsinst",
-		Usage: `aws instance name`,
+		Usage: `AWS instance name`,
 	}
 
 	InfluxdbEnabledFlag = cli.BoolFlag{
 		Name:  "influxdb",
-		Usage: `influxdb enabled`,
+		Usage: `Influxdb enabled`,
 	}
 
 	InfluxdbEndpointFlag = cli.StringFlag{
 		Name:  "influxdb.endpoint",
-		Usage: `influxdb endpoint`,
+		Usage: `Influxdb endpoint`,
 	}
 
 	InfluxdbTokenFlag = cli.StringFlag{
 		Name:  "influxdb.token",
-		Usage: `influxdb token or username:password`,
+		Usage: `Influxdb token or username:password`,
 		Value: ":",
 	}
 	InfluxdbOrgFlag = cli.StringFlag{
 		Name:  "influxdb.org",
-		Usage: `influxdb org name`,
+		Usage: `Influxdb org name`,
 		Value: "",
 	}
 	InfluxdbBucketFlag = cli.StringFlag{
 		Name:  "influxdb.bucket",
-		Usage: `influxdb bucket or database name`,
+		Usage: `Influxdb bucket or database name`,
 	}
 	InfluxdbPointNameFlag = cli.StringFlag{
 		Name:  "influxdb.point",
-		Usage: `influxdb point name`,
+		Usage: `Influxdb point name`,
 		Value: "quorumTpsMon",
 	}
 	InfluxdbTagsFlag = cli.StringFlag{
 		Name:  "influxdb.tags",
-		Usage: `influxdb tags (comma separated list of key=value pairs)`,
+		Usage: `Influxdb tags (comma separated list of key=value pairs)`,
 		Value: "system=quorum,comp=tps",
 	}
 	ConsensusFlag = cli.StringFlag{
 		Name:  "consensus",
-		Usage: `name of consensus ("raft", "ibft")`,
+		Usage: `Name of consensus ("raft", "ibft")`,
 	}
 
 	DebugFlag = cli.BoolFlag{
 		Name:  "debug",
-		Usage: `debug mode`,
+		Usage: `Debug mode`,
 	}
 
 	HttpEndpointFlag = cli.StringFlag{
 		Name:  "httpendpoint",
-		Usage: "geth chainReader's http endpoint",
+		Usage: "Geth's http endpoint",
 	}
 
 	ReportFileFlag = cli.StringFlag{
 		Name:  "report",
-		Usage: "full path of the file to write the report",
+		Usage: "Full path of csv report file",
 		Value: "tps-report.csv",
 	}
 
 	TpsPortFlag = cli.IntFlag{
 		Name:  "port",
-		Usage: "port for tps monitor",
+		Usage: "Http port for tps monitor",
 		Value: 7575,
 	}
 
 	FromBlockFlag = cli.Uint64Flag{
 		Name:  "from",
-		Usage: "from block no",
+		Usage: "From block no",
 		Value: 0,
 	}
 
 	ToBlockFlag = cli.Uint64Flag{
 		Name:  "to",
-		Usage: "to block no",
+		Usage: "To block no",
 		Value: 0,
 	}
 
