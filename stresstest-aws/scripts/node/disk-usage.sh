969190c61e82c11a3f908b1f44a25439846eb62d
Baptiste Boussemart <baptiste.boussemart@consensys.net>
2021-07-28 13:01:39 +0200

    fix: remove \n as echo add one

--- stresstest-aws/scripts/node/disk-usage.sh
+++ stresstest-aws/scripts/node/disk-usage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while read -a LIST; \
 do timestamp=$(date +%s%N); \
-        echo 'disk-usage,folder="'${LIST[1]}'" bytes='${LIST[0]}' '$timestamp'\n'; \
+        echo 'disk-usage,folder="'${LIST[1]}'" bytes='${LIST[0]}' '$timestamp; \
 done < <(du -b /data/tm/db.mv.db /data/tm/db.trace.db /data/qdata/geth/chaindata /data/qdata/geth/lightchaindata /data/qdata/geth/nodes /data/qdata/geth/transactions.rlp 2> /dev/null) 
